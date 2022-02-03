07f30865c065694fe0b2670938dba6eb6869c9e4
Akshith Sagar <akshith.sagar7@gmail.com>
2022-02-03 02:15:24 -0500

    Adding Components

--- FrontEnd/FE-Sprint1/src/app/app.module.ts
+++ FrontEnd/FE-Sprint1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddMovieComponent
   ],
   imports: [
     BrowserModule,
