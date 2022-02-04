5c06dbe0c5c8b0305015ba6e27fd1677e6f95609
Akshith Sagar <akshith.sagar7@gmail.com>
2022-02-04 15:00:16 -0500

    Adding routes

--- FrontEnd/FE-Sprint1/src/app/add-movies/add-movies.component.ts
+++ FrontEnd/FE-Sprint1/src/app/add-movies/add-movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add-movies',
   templateUrl: './add-movies.component.html',
--- FrontEnd/FE-Sprint1/src/app/app-routing.module.ts
+++ FrontEnd/FE-Sprint1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddMoviesComponent } from './add-movies/add-movies.component';
+import { HeaderComponent } from './header/header.component';
+const routes: Routes = [
+  {path:'headercomponent',component: HeaderComponent},
+  {path:'addmoviecomponent',component: AddMoviesComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
