5e89e0e766742c0dc95eec0adec508a194b877d0
DhanushUF <38401094+Dhanush05@users.noreply.github.com>
2022-02-04 04:58:23 -0500

    Home Page - Implemented movie suggestions
    Movie names, images and descriptions are dynamic. Details and top-discussions yet to be added.

--- FrontEnd/FE-Sprint1/src/app/home-page/top-suggestions/suggestion-list/suggestion-list.component.ts
+++ FrontEnd/FE-Sprint1/src/app/home-page/top-suggestions/suggestion-list/suggestion-list.component.ts
@@ -9,7 +9,10 @@ import { Suggestions } from '../top-suggestion.model';
 })
 export class SuggestionListComponent implements OnInit {
   suggestions: Suggestions[] = [
-    new Suggestions('Test Name1','Test DEscription', 'https://m.media-amazon.com/images/I/61aG6EicTIL._AC_SY741_.jpg')
+    new Suggestions('Test Name1','Test Description 1', 'https://m.media-amazon.com/images/I/61aG6EicTIL._AC_SY741_.jpg'),
+    new Suggestions('Test Name2', 'Test Description 2', 'https://m.media-amazon.com/images/I/91zzAMkVCUL._AC_SL1500_.jpg'),
+    new Suggestions('Test Name3','Test Description 3', 'https://m.media-amazon.com/images/I/61aG6EicTIL._AC_SY741_.jpg'),
+    new Suggestions('Test Name4', 'Test Description 4', 'https://m.media-amazon.com/images/I/91zzAMkVCUL._AC_SL1500_.jpg')
   ];
   constructor() { }
 
