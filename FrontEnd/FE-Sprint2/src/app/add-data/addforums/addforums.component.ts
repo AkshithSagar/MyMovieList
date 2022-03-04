10594127b2c148b8c1ed5baab2201822308cc5c8
Akshith Sagar <akshith.sagar7@gmail.com>
2022-03-04 05:21:56 -0500

    Update addforums.component.ts

--- FrontEnd/FE-Sprint2/src/app/add-data/addforums/addforums.component.ts
+++ FrontEnd/FE-Sprint2/src/app/add-data/addforums/addforums.component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-addforums',
   templateUrl: './addforums.component.html',
   styleUrls: ['./addforums.component.scss']
 })
-export class AddforumsComponent {
+export class AddforumsComponent implements OnInit {
   constructor(private fromBuilder:FormBuilder) { }
-
-  profileForm = this.fromBuilder.group({
-    moviename:[''],
-    description:['']
-    // review:[''],
-    // rating:['']
-  });
-  onSubmit(){
-    console.log('form data is', this.profileForm.value);
-    console.log('save working');
-    //write back end code to push form data to database..
-    this.profileForm.reset();
+  profileForm: FormGroup;
+  ngOnInit(): void {
+    this.profileForm = this.fromBuilder.group({
+      moviename:['',[Validators.required]],
+      description:['']
+      // review:[''],
+      // rating:['']
+    });
   }
   
-  
+  onSubmit(){
+    if(this.profileForm.valid){
+      console.log('form data is', this.profileForm.value);
+      console.log('save working');
+      //write back end code to push form data to database..
+      this.profileForm.reset();
+    }
+    
+  }
+ 
 }
