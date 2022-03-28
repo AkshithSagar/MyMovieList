40b29ae1ea1e86d53ca4dfc85020cd5380b0c214
DhanushUF <38401094+Dhanush05@users.noreply.github.com>
2022-03-28 02:00:34 -0400

    Form validations to login and signup
    To do: check regular expression and hints

--- FrontEnd/FE-Sprint3/src/app/login/login.component.ts
+++ FrontEnd/FE-Sprint3/src/app/login/login.component.ts
@@ -9,9 +9,22 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+  profileForm: FormGroup;
 
   ngOnInit(): void {
+    this.profileForm = this.formBuilder.group({
+      username:['',[Validators.required,Validators.maxLength(25)]],
+      password:['',[Validators.required, Validators.maxLength(32)]]
+    })
+  }
+  onSubmit(){
+    if(this.profileForm.valid){
+      console.log('form data is', this.profileForm.value);
+      console.log('working');
+      this.profileForm.reset();
+      
+    }
   }
 
 }
