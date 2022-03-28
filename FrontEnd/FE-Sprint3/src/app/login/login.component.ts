fdd2ea515694d5c8a81ced7630ea70c5bff92d39
DhanushP <38401094+Dhanush05@users.noreply.github.com>
2022-03-28 02:06:10 -0400

    Revert "Form validations to login and signup"

--- FrontEnd/FE-Sprint3/src/app/login/login.component.ts
+++ FrontEnd/FE-Sprint3/src/app/login/login.component.ts
@@ -9,22 +9,9 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
-  profileForm: FormGroup;
+  constructor() { }
 
   ngOnInit(): void {
-    this.profileForm = this.formBuilder.group({
-      username:['',[Validators.required,Validators.maxLength(25)]],
-      password:['',[Validators.required, Validators.maxLength(32)]]
-    })
-  }
-  onSubmit(){
-    if(this.profileForm.valid){
-      console.log('form data is', this.profileForm.value);
-      console.log('working');
-      this.profileForm.reset();
-      
-    }
   }
 
 }
