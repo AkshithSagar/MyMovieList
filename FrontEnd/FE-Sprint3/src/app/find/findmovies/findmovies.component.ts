de1528127015c876af06f0c75c8f74cc848625d1
Akshith Sagar <akshith.sagar7@gmail.com>
2022-03-29 00:44:36 -0400

    Added search bar for find functionality

---
+++ FrontEnd/FE-Sprint3/src/app/find/findmovies/findmovies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-findmovies',
+  templateUrl: './findmovies.component.html',
+  styleUrls: ['./findmovies.component.scss']
+})
+export class FindmoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
