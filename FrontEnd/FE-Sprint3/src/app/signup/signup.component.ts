2b299247eef02265cf68316e4e3fa4d279f1c4b3
DhanushUF <38401094+Dhanush05@users.noreply.github.com>
2022-03-29 00:46:14 -0400

    Working on http connection

--- FrontEnd/FE-Sprint3/src/app/signup/signup.component.ts
+++ FrontEnd/FE-Sprint3/src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,7 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http:HttpClient) { }
   profileForm : FormGroup;
 
   ngOnInit(): void {
@@ -22,6 +22,16 @@ export class SignupComponent implements OnInit {
     })
   }
   onSubmit(){
+    var formData: any = new FormData();
+    formData.append("email", this.profileForm.get('email').value);
+    formData.append("username", this.profileForm.get('username').value);
+    formData.append("password", this.profileForm.get('password').value);
+    formData.append("question", this.profileForm.get('question').value);
+    formData.append("answer", this.profileForm.get('answer').value);
+    formData.append("birthday", this.profileForm.get('birthday').value);
+    for(var value of formData.values()){
+      console.log(value);
+    }
     if(this.profileForm.valid){
       console.log('signup data is', this.profileForm.value);
       console.log('working');
