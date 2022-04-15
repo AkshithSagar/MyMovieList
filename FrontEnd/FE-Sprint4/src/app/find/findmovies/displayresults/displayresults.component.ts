175c7838f8568689e1ca43af777d3015afa9062e
DhanushUF <38401094+Dhanush05@users.noreply.github.com>
2022-04-15 16:55:26 -0400

    Rest APi to display results componets
    Successfully displays the table with filter after migrating the functionality to api call service.

--- FrontEnd/FE-Sprint4/src/app/find/findmovies/displayresults/displayresults.component.ts
+++ FrontEnd/FE-Sprint4/src/app/find/findmovies/displayresults/displayresults.component.ts
@@ -2,39 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import { ApiCallService } from 'src/app/api-call.service';
 
-export interface Elements{
+export interface Element{
   Name: string;
   Rating: number;
   Genre: string;
+  Desc: string;
+  Review: string;
+  ID: string;
 }
-const ELEMENT_DATA: Elements[] = [
-  {Name:'Harry Potter',Rating:5,Genre:'Adventure'},
-  {Name:'Pirates of the sea',Rating:3,Genre:'Adventure'},
-  {Name:'Avengers',Rating:5,Genre:'Action'},
-  {Name:'Interstellar',Rating:5,Genre:'Sci-Fi'},
-  {Name:'La la land',Rating:3,Genre:'Romance'},
-  {Name:'One Piece',Rating:5,Genre:'Anime'},  
-];
+
+// const ELEMENT_DATA: Element[] = [
+//   {Name:'Harry Potter',Rating:5,Genre:'Adventure'},
+//   {Name:'Pirates of the sea',Rating:3,Genre:'Adventure'},
+//   {Name:'Avengers',Rating:5,Genre:'Action'},
+//   {Name:'Interstellar',Rating:5,Genre:'Sci-Fi'},
+//   {Name:'La la land',Rating:3,Genre:'Romance'},
+//   {Name:'One Piece',Rating:5,Genre:'Anime'},  
+// ];
+
 @Component({
   selector: 'app-displayresults',
   templateUrl: './displayresults.component.html',
   styleUrls: ['./displayresults.component.scss']
 })
-export class DisplayresultsComponent  {
-  private data:any = [];
-  
+
+export class DisplayresultsComponent implements OnInit {
+  private data:any = []
+  tabdata: MatTableDataSource<Element>;
+  dataSource: MatTableDataSource<Element>;
+
   constructor(private getapi: ApiCallService) { }
   ngOnInit(){
+    this.getAllValues();
+  }  
+  getAllValues(){
     this.getapi.getPosts().subscribe((results)=>{
+      console.log(results)
       //console.warn("result",results)
-      this.data=results
+      //this.data=results
+      //this.tabdata = new MatTableDataSource(<any>results);
+      this.dataSource = new MatTableDataSource(<any>results);
+
       console.log(this.data)
-      console.log(ELEMENT_DATA)
+      console.log(this.tabdata)
+      //console.log(ELEMENT_DATA)
     })
 
-  }  
+  }
+  
   displayedColumns: string[] = ['Name', 'Rating', 'Genre'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  //dataSource = new MatTableDataSource(this.data);
+  
+  
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
