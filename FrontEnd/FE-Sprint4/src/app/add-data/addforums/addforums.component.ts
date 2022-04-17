fb0f931ab07aad065e9ced2f3f0b91d5fa5812ae
Akshith Sagar <akshith.sagar7@gmail.com>
2022-04-17 14:05:38 -0400

    Final commit to add discussion page
    Verifies UI Components, Backend integration, Testing and working of the page. No more edits needed

--- FrontEnd/FE-Sprint4/src/app/add-data/addforums/addforums.component.ts
+++ FrontEnd/FE-Sprint4/src/app/add-data/addforums/addforums.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
@@ -6,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./addforums.component.scss']
 })
 export class AddforumsComponent implements OnInit {
-  constructor(private fromBuilder:FormBuilder) { }
+  constructor(private fromBuilder:FormBuilder,private http:HttpClient) { }
   profileForm: FormGroup;
   ngOnInit(): void {
     this.profileForm = this.fromBuilder.group({
@@ -22,6 +23,19 @@ export class AddforumsComponent implements OnInit {
       console.log('form data is', this.profileForm.value);
       console.log('save working');
       //write back end code to push form data to database..
+      var formData:any = new FormData();
+      formData.append("TopicName",this.profileForm.get('moviename').value);
+      formData.append("Description",this.profileForm.get('description').value);
+      var object={}
+      formData.forEach((value,key)=>object[key]=value);
+      var json = JSON.stringify(object);
+      console.log(json)
+      this.http.post('http://localhost:3000/addD',json)
+      .subscribe({
+        next:(response)=>{
+          console.log(response)
+        }
+      });
       this.profileForm.reset();
     }
     
