e3363e2abb10b6bf2cb68417e9c0e52f986f3d33
DhanushUF <38401094+Dhanush05@users.noreply.github.com>
2022-04-19 17:57:19 -0400

    Search bar in find movies working

--- FrontEnd/FE-Sprint4/src/app/find/findmovies/findmovies.component.ts
+++ FrontEnd/FE-Sprint4/src/app/find/findmovies/findmovies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/sharing/data.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
@@ -9,13 +9,18 @@ import { Router } from '@angular/router';
 })
 export class FindmoviesComponent implements OnInit {
   //
+  @ViewChild('ce') ce: ElementRef;
   message:string;
+  message2:string;
   subscription: Subscription;
+  subscription2: Subscription;
+  searchmsg:string;
   //
   constructor(private data: DataService, private router: Router) { }
   parent:string = null;
   ngOnInit(){
     this.subscription= this.data.currentMessage.subscribe(message=>this.message = message)
+    this.subscription2 = this.data.selectedMovie.subscribe(message=>this.message2 = message)
   }
   public allMovies():void{
     this.parent = "AllMovies";
@@ -55,6 +60,18 @@ export class FindmoviesComponent implements OnInit {
     this.data.changeMessage("Thriller")
     this.router.navigate(['moviebygenre'])
   }
+  searchBar(){
+    //console.log(this.ce.nativeElement.value)
+    this.searchmsg = this.ce.nativeElement.value;
+    console.log(this.searchmsg);
+    this.data.selectMovie(this.searchmsg);
+    this.router.navigate(['moviedetails']);    
+  }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+    this.subscription2.unsubscribe();
 
+  }
+  
  
 }
