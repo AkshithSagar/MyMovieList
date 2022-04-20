cb8ffd99b2daa0000dead7bd74b600bbfa54ded8
Akshith <dhanushpakanati@gmail.com>
2022-04-20 04:05:20 -0400

    added navigation to display movie details from home.

--- FrontEnd/FE-Sprint4/src/app/home/suggestions/suggestions.component.ts
+++ FrontEnd/FE-Sprint4/src/app/home/suggestions/suggestions.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { element } from 'protractor';
+import { Subscription } from 'rxjs';
 import { ApiCallService } from 'src/app/api-call.service';
+import { DataService } from 'src/app/sharing/data.service';
 import { Suggestions } from './suggestions.model';
 import { User } from './User.model';
 @Component({
@@ -9,14 +13,20 @@ import { User } from './User.model';
 })
 
 export class SuggestionsComponent implements OnInit {
-
+  subscription: Subscription;
+   message: string;
+   msg:string;
+   len:number;
+  
   suggestions = []
-  constructor(private getapi: ApiCallService) {
+  constructor(private getapi: ApiCallService, private router: Router,
+    private dataservice: DataService) {
     
    }
   
   ngOnInit(): void {
     this.diplaymovies();
+    this.subscription = this.dataservice.selectedMovie.subscribe(message=>this.msg= this.message)
   }
   diplaymovies(){
     this.getapi.getTopFiveMovies().subscribe((results)=>{
@@ -28,15 +38,22 @@ export class SuggestionsComponent implements OnInit {
       // for(var counter:number =0;counter<5;counter++){
       //   this.suggestions.push(new Suggestions(JSON.stringify(results[counter]["Name"]),JSON.stringify(results[counter]["Genre"]), JSON.stringify(results[counter]["Desc"])  , 'https://m.media-amazon.com/images/I/91BT--NUiKL._AC_UY436_FMwebp_QL65_.jpg'))
       // }
-      this.suggestions.push(new Suggestions(JSON.stringify(results[0]["Name"]),JSON.stringify(results[0]["Genre"]), JSON.stringify(results[0]["Review"])  , 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy99-JIoQD_tDXwVlktYQVhmWUZPAVX92Tq4FIssNpJ9WDQYrP'))
-      this.suggestions.push(new Suggestions(JSON.stringify(results[1]["Name"]),JSON.stringify(results[1]["Genre"]), JSON.stringify(results[1]["Review"])  , 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS5uxkfEsqCe-oN5gfCK0jE2FiXlntNoYuyNmookn5PBBMTV5Yj'))
-      this.suggestions.push(new Suggestions(JSON.stringify(results[2]["Name"]),JSON.stringify(results[2]["Genre"]), JSON.stringify(results[2]["Review"])  , 'https://m.media-amazon.com/images/I/91BT--NUiKL._AC_UY436_FMwebp_QL65_.jpg'))
-      this.suggestions.push(new Suggestions(JSON.stringify(results[3]["Name"]),JSON.stringify(results[3]["Genre"]), JSON.stringify(results[3]["Review"])  , 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy99-JIoQD_tDXwVlktYQVhmWUZPAVX92Tq4FIssNpJ9WDQYrP'))
-      this.suggestions.push(new Suggestions(JSON.stringify(results[4]["Name"]),JSON.stringify(results[4]["Genre"]), JSON.stringify(results[4]["Review"])  , 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZhbU5pWXmpsBxgV6PFL-kAxVAihDFPueGh0n2HoUbWZdFp_AC'))
-
-
-
+      this.suggestions.push(new Suggestions(JSON.stringify(results[0]["Name"]),JSON.stringify(results[0]["Genre"]), JSON.stringify(results[0]["Review"])  , 'https://upload.wikimedia.org/wikipedia/en/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg'))
+      this.suggestions.push(new Suggestions(JSON.stringify(results[1]["Name"]),JSON.stringify(results[1]["Genre"]), JSON.stringify(results[1]["Review"])  , 'https://m.media-amazon.com/images/I/91BT--NUiKL._AC_UY436_FMwebp_QL65_.jpg'))
+      this.suggestions.push(new Suggestions(JSON.stringify(results[2]["Name"]),JSON.stringify(results[2]["Genre"]), JSON.stringify(results[2]["Review"])  , 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQXGJJq9ioEIztF_HSvK0fnX4UMKjcnEaEsg_WHbGJOeAf3uwft'))
+      this.suggestions.push(new Suggestions(JSON.stringify(results[3]["Name"]),JSON.stringify(results[3]["Genre"]), JSON.stringify(results[3]["Review"])  , 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUw076GR7JpnfExoBLTMpiE9otUzk518SylUdC1roF6Ah63NS9'))
+      this.suggestions.push(new Suggestions(JSON.stringify(results[4]["Name"]),JSON.stringify(results[4]["Genre"]), JSON.stringify(results[4]["Review"])  , 'https://m.media-amazon.com/images/M/MV5BMTc3NjI2MjU0Nl5BMl5BanBnXkFtZTgwNDk3ODYxMTE@._V1_FMjpg_UX1000_.jpg'))
     })
+  }
+  getMovieDetails(element){
+    this.message = element.innerHTML;
+    this.len = this.message.length;
+    console.log(this.len)
+    this.message = this.message.substring(1,this.len-1)
+    console.log(this.message)
+    this.dataservice.selectMovie(this.message)
+    this.router.navigate(['moviedetails'])
+      
   }
 
   // suggestions: Suggestions[]=[
